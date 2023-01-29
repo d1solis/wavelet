202abe9d883087dbae4d45a5e90c9dfb4ebb416c
d1solis <d1solis@ucsd.edu>
2023-01-29 00:06:08 -0800

    Create StringServer.java

---
+++ StringServer.java
@@ -0,0 +1,38 @@
+import java.io.IOException;
+import java.net.URI;
+import java.util.ArrayList;
+
+class StringHandler implements URLHandler {
+    // The one bit of state on the server: a string that will be manipulated by
+    // various requests.
+    ArrayList <String> singleString = new ArrayList <String>();
+    String serverString = "";
+
+    public String handleRequest(URI url) {
+        if (url.getPath().equals("/")) {
+            return String.format("Starting...");
+
+        } else if (url.getPath().contains("/add")) {
+            String[] parameters = url.getQuery().split("=");
+            singleString.add(parameters[1]);
+            return String.format("Added " + parameters[1]);
+
+        } else {
+            System.out.println("Path: " + url.getPath());
+            return "404 Not Found!";
+        }
+    }
+}
+
+class StringServer {
+    public static void main(String[] args) throws IOException {
+        if(args.length == 0){
+            System.out.println("Missing port number! Try any number between 1024 to 49151");
+            return;
+        }
+
+        int port = Integer.parseInt(args[0]);
+
+        Server.start(port, new Handler());
+    }
+}

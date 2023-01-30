b92355233a5ee9fc5636c37d449cfda5e4ad156f
d1solis <d1solis@ucsd.edu>
2023-01-30 14:56:10 -0800

    Update StringServer.java

--- StringServer.java
+++ StringServer.java
@@ -1,21 +1,25 @@
 import java.io.IOException;
 import java.net.URI;
-import java.util.ArrayList;
 
 class StringHandler implements URLHandler {
     // The one bit of state on the server: a string that will be manipulated by
     // various requests.
-    ArrayList <String> singleString = new ArrayList <String>();
     String serverString = "";
 
     public String handleRequest(URI url) {
         if (url.getPath().equals("/")) {
             return String.format("Starting...");
 
-        } else if (url.getPath().contains("/add")) {
-            String[] parameters = url.getQuery().split("=");
-            singleString.add(parameters[1]);
-            return String.format("Added " + parameters[1]);
+        } else if (url.getPath().contains("/add-message")) {
+            String[] val1 = url.getQuery().split("=");
+
+            if(val1[0].equals("s")){
+                serverString += "\n" + val1[1];
+                return serverString;
+            }
+            else{
+                return "404 Not Found!";
+            }
 
         } else {
             System.out.println("Path: " + url.getPath());
@@ -33,6 +37,6 @@ class StringServer {
 
         int port = Integer.parseInt(args[0]);
 
-        Server.start(port, new Handler());
+        Server.start(port, new StringHandler());
     }
 }
